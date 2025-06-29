8071c4063834462566df5f68719791748e9c9ef5
MARC NAVARRO <navamarc@gmail.com>
2025-06-29 06:01:11 +0200

    update to entity to columninfo, converter class, primary key

--- app/src/main/java/com/openclassrooms/petisoin/data/entity/Animal.kt
+++ app/src/main/java/com/openclassrooms/petisoin/data/entity/Animal.kt
@@ -1,11 +1,27 @@
 package com.openclassrooms.petisoin.data.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "animal")
 data class Animal(
+  @PrimaryKey(autoGenerate = true)
   val id: Long = 0,
+
+  @ColumnInfo(name = "type")
   val type: Type = Type.CAT,
+
+  @ColumnInfo(name = "race")
   val race: Race = Race.PERSIAN,
+
+  @ColumnInfo(name = "weight")
   val weight: Double = 0.0,
+
+  @ColumnInfo(name = "height")
   val height: Int = 0,
+
+  @ColumnInfo(name = "color")
   val color: String = ""
 )
 
